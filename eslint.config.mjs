fdbe63194152f7dae1a64f1074b5b8411c4dee01
Nitin Sudarsanam <nitinsudarsanam@yahoo.com>
2025-04-14 21:57:51 -0400

    buld better

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // turn off some annoying rules
+      'no-console': 'off',
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-img-element': 'off',
+      "@next/next/no-html-link-for-pages": "off"
+    },
+  },
 ];
 
 export default eslintConfig;
